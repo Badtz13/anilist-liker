087c6dfac11a2d6878fff2e3c895dc8e22e3504b
riverrrrrr <riverehill@gmail.com>
2020-06-17 18:19:22 -0700

    added firefox feature
    Now clicking on the icon will open anilist if the current tab is not on anilist

--- Firefox/background.js
+++ Firefox/background.js
@@ -1,5 +1,13 @@
-function likePosts() {
-    browser.tabs.executeScript(null, {file: "content.js"});
+function likePostsOrOpenTab() {
+    function logTabs(tabs) {
+        let tab = tabs[0];
+
+        if(tab.url.includes('anilist.co'))
+            browser.tabs.executeScript(null, {file: "content.js"});
+        else
+            browser.tabs.create({url: "https://www.anilist.co"});
+    }   
+    browser.tabs.query({currentWindow: true, active: true}).then(logTabs, console.error);
 }
 
-browser.browserAction.onClicked.addListener(likePosts);
+browser.browserAction.onClicked.addListener(likePostsOrOpenTab);
