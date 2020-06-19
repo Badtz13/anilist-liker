b1a5c5c7715447490215c796a53c5bae49af0420
riverrrrrr <riverehill@gmail.com>
2020-06-18 21:58:40 -0700

    added chrome feature

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,3 +1,11 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, {file: "content.js"});
- });
+function likePostsOrOpenTab() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        if(url.includes('anilist.co'))
+            chrome.tabs.executeScript(null, {file: "content.js"});
+        else
+            chrome.tabs.create({url: "https://www.anilist.co"});
+    });
+}
+
+chrome.browserAction.onClicked.addListener(likePostsOrOpenTab);
