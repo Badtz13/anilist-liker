12b8801bc89b8d949a2aae5da32a14deff23e8f3
Badtz <badtzmaru139@gmail.com>
2021-12-14 16:43:13 -0800

    block liking of own posts

--- Chrome/content.js
+++ Chrome/content.js
@@ -1,4 +1,9 @@
+var username = document.getElementsByClassName("links")[0].getElementsByClassName("link")[1].href
+
 var likes = document.querySelectorAll(".button:not(.liked)");
-for(var i = 0; likes.length; i++) {
-    likes[i].click();
+
+for (var i = 0; likes.length; i++) {
+    if (username != likes[i].parentElement.parentElement.parentElement.parentElement.getElementsByClassName("avatar")[0].href) {
+        likes[i].click();
+    }
 }
